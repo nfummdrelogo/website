ae53c6a0669823b7845f1ecf7b16e2667a6f5927
Hans Chiu <chiuhans111@gmail.com>
2019-10-19 15:07:25 +0800

    initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+頁面在趕工中
+</div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+        
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+    
+</style>
