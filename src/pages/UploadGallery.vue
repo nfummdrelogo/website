b173e4e41c4a3f3aa109bdcb1177f28c492ebd7e
Hans Chiu <chiuhans111@gmail.com>
2019-10-20 14:41:28 +0800

    gallery

--- src/pages/UploadGallery.vue
+++ src/pages/UploadGallery.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
-    <div>
-      <div>
-        <div class="container">
-          <h2>作品牆面</h2>
-          <h4>RELOGO</h4>
-          <div class="gallery-grid">
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-            <div class="gallery-item"></div>
-          </div>
-        </div>
+    <div class="container">
+      <h2>作品牆面</h2>
+      <h4>RELOGO</h4>
+      <div class="gallery-grid">
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
+        <div class="gallery-item"></div>
       </div>
+      <transition name="page" type="out-in">
+        <div class="cover">
+          <div class="msg">即將於10/25開放</div>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -33,7 +34,29 @@ export default {
 
 
 <style lang="scss" scoped>
+.cover {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  top: 0px;
+  left: 0px;
+  z-index: 0;
+}
 
-
-
+.msg {
+  position: relative;
+  margin: auto;
+  top: 50%;
+  transform: translate(0, -50%);
+  width: 596px;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  font-size: 48px;
+  font-weight: 800;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: 19.92px;
+  color: #ffffff;
+}
 </style>
