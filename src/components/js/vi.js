f028a753dad23e733b199e1bd54b29384f27d998
Hans Chiu <chiuhans111@gmail.com>
2019-10-20 10:28:38 +0800

    vi tweak

--- src/components/js/vi.js
+++ src/components/js/vi.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import model from "@/assets/vi/3DMODEL.glb";
+import model from "@/assets/vi/3DMODEL2.glb";
 import map1 from "@/assets/vi/maps/Right.png";
 import map2 from "@/assets/vi/maps/Left.png";
 import map3 from "@/assets/vi/maps/Top.png";
@@ -28,12 +28,12 @@ export default {
     renderer.setClearAlpha(0)
     renderer.clearColor()
 
-    
+
     // renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.gammaOutput = true;
-    renderer.gammaFactor = 1;
-    
-    // scene.add(new THREE.AmbientLight(0xffffff, 1))
+    renderer.gammaFactor = 2.2;
+
+    scene.add(new THREE.AmbientLight(0xffffff, 1))
 
     var textureCube = cubeTextureLoader.load([
       map1, map2, map3, map4, map5, map6
@@ -41,7 +41,7 @@ export default {
 
 
 
-    
+
 
 
     var target = new THREE.Mesh()
@@ -53,12 +53,16 @@ export default {
         target = obj
 
         if (obj.type == 'Mesh') {
-          obj.material = new THREE.MeshPhysicalMaterial({
-            map: obj.material.map,
-            metalness  : 6,
-            envMap: textureCube,
-            clearcoat: 1,
-          })
+          // obj.material = new THREE.MeshPhysicalMaterial({
+          //   map: obj.material.map,
+          //   metalness: 6,
+          //   envMap: textureCube,
+          //   clearcoat: 1,
+          //   shading: THREE.SmoothShading
+          // })
+          obj.material.envMap = textureCube
+          obj.material.envMapIntensity = 2
+          obj.material.metalness = .99
           obj.scale.y = 1.5
           scene.add(obj)
         }
@@ -74,7 +78,7 @@ export default {
     var x = 0;
     var y = 0;
 
-    function move(ex, ey){
+    function move(ex, ey) {
       x = ex / innerWidth - 0.5
       y = ey / innerHeight - 0.5
     }
@@ -84,30 +88,34 @@ export default {
       mouseMove: function (e) {
         move(e.pageX, e.pageY)
       },
-      touchmove: function(e){
+      touchmove: function (e) {
         move(e.changedTouches[0].pageX, e.changedTouches[0].pageY)
-      }
+      },
+      click: function () {
+        target.rotation.y = -2*Math.PI
+      }, canvas
     }
 
     addEventListener('mousemove', animationObj.mouseMove)
     addEventListener('touchmove', animationObj.touchmove)
+    addEventListener('click', animationObj.click)
 
     function update() {
       // console.log('run')
       if (animationObj.stop) return;
       requestAnimationFrame(update);
 
-      var tx = -x * 3 + 3
-      var ty = y * 3 - 2
-      var tz = Math.sqrt(80 - tx * tx - ty * ty)
+      var tx = -x * 4 + 2
+      var ty = y * 4 - 1.5
+      var tz = Math.sqrt(Math.abs(80 - tx * tx - ty * ty))
 
       target.rotation.y *= 0.985
       target.rotation.x *= 0.985
 
 
-      camera.position.x += (tx - camera.position.x) * 0.035
-      camera.position.y += (ty - camera.position.y) * 0.035
-      camera.position.z += (tz - camera.position.z) * 0.035
+      camera.position.x += (tx - camera.position.x) * 0.031
+      camera.position.y += (ty - camera.position.y) * 0.031
+      camera.position.z += (tz - camera.position.z) * 0.031
 
       camera.lookAt(0, 0, 0)
 
@@ -128,6 +136,7 @@ export default {
     animationObj.stop = true
     removeEventListener('mousemove', animationObj.mouseMove)
     removeEventListener('touchmove', animationObj.touchmove)
+    removeEventListener('click', canvas.click)
 
   }
 }
