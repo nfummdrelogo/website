b7a148ee533db52cd2ad71ba80c6d7792c98d48b
Hans Chiu <chiuhans111@gmail.com>
2019-11-25 22:05:19 +0800

    open image

--- src/pages/UploadGallery.vue
+++ src/pages/UploadGallery.vue
@@ -14,8 +14,24 @@
             v-bind:style="{
             backgroundImage: 'url('+item.img+')'
             }"
+            @click="selected=item"
           ></div>
         </template>
+      </div>
+      <div class="popout" v-if="selected!=null">
+        <div class="expand">
+          <div class="scroll">
+            <div class="container">
+              <div class="img">
+                <img :src="selected.img" alt />
+                <div class="close" @click="selected=null">X</div>
+              </div>
+              <h4>作品名稱 - {{selected.name}}</h4>
+              <h4>設計理念 -</h4>
+              <p>{{selected.desc}}</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="cover" :hidden="show">
         <div class="msg">即將於10/25開放</div>
@@ -42,7 +58,8 @@ export default {
     return {
       items: [],
       show: true,
-      status: ""
+      status: "",
+      selected: null
     };
   },
   mounted() {
@@ -88,6 +105,68 @@ export default {
   top: 0px;
   left: 0px;
   z-index: 0;
+}
+
+.popout {
+  .expand {
+    padding-top: 80px;
+
+    margin-right: -50px;
+    position: relative;
+    height: 100%;
+    overflow: auto;
+  }
+
+  .scroll {
+    padding-right: 50px;
+  }
+
+  position: fixed;
+  padding-left: 75px;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  color: white;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  @include rwd-dn(phone) {
+    padding-left: 0px;
+  }
+
+  h4 {
+    color: white;
+    font-weight: bold;
+  }
+  .img {
+    position: relative;
+    width: 660px;
+    max-width: 100%;
+    background-size: cover;
+
+    img {
+      width: 100%;
+    }
+
+    .close {
+      opacity: 0.5;
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 40px;
+      height: 40px;
+      background-color: rgba(196, 196, 196, 0.65);
+      padding-left: 12px;
+      padding-top: 8px;
+      font-size: 24px;
+      cursor: pointer;
+      transition: opacity 0.5s;
+    }
+
+    &:hover .close {
+      opacity: 1;
+    }
+  }
 }
 
 .msg {
